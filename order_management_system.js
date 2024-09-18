6b2db05cc9d460811e8a45273881413233cf1e9c
Mamun Salem <salemtwin2@gmail.com>
2024-09-17 21:11:27 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,10 @@
+//Customer Order Management 
+
+//Task 1- Create an Inventory Array of Product Objects
+const inventory = [
+    { name: 'Caramel Frappe', price: 3, quantity: 10 },
+    { name: 'Iced Caramel Macchiato', price: 4, quantity: 5 },
+    { name: 'Black Coffee', price: 4, quantity: 6 },
+    { name: 'Arabian Coffee', price: 5, quantity: 4 }
+];
+
